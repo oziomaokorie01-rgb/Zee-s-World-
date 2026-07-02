00276e5d240bf77b3af2d94ddd1d3bfbdb66fcca
Ozzy-sensei <oziomaokorie01@gmail.com>
2026-07-02 11:57:14 +0100

    Add CSS3DButterfly component to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useIntroStore } from './store/useIntroStore';
 import CinematicIntro from './components/CinematicIntro';
 import NarrativeHUD from './components/NarrativeHUD';
 import WorldCanvas from './components/WorldCanvas';
+import CSS3DButterfly from './components/CSS3DButterfly';
 import UIOverlay from './components/UIOverlay';
 import ProjectDrawer from './components/ProjectDrawer';
 import TourGuideOverlay from './components/TourGuideOverlay';
@@ -23,6 +24,7 @@ export default function App() {
       <>
       <NarrativeHUD />
         <WorldCanvas />
+        <CSS3DButterfly />
       </>
       ) : (
         <div className="absolute inset-0 bg-radial-gradient from-[#0f0720] to-[#05020a] z-0" />
